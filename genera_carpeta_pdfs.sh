aed5c3681e3db2f06701e60409067eb1a0df76a7
Jose A. Murcia <joamuran@gmail.com>
2025-11-27 09:36:53 +0100

    Afegides AOF, ASO i ISO

--- genera_carpeta_pdfs.sh
+++ genera_carpeta_pdfs.sh
@@ -2,7 +2,7 @@
 
 # Directori d'origen
 source_dir="Programacions"
-source_hardcoded_dir="pdfs_extra"
+source_hardcoded_dir="pdfs_extra/*"
 
 # Directori de destinació
 destination_dir="PDFs"
@@ -10,10 +10,18 @@ destination_dir="PDFs"
 # Crear la carpeta PDFs si no existeix
 mkdir -p "$destination_dir"
 
+
+# Afegim les programacions extra
+#mkdir -p "${destination_dir}/${source_dir}"
+#cp -r "${source_hardcoded_dir}" "${destination_dir}/${source_dir}/"
+
+
 # Buscar i copiar tots els fitxers .pdf mantenint l'estructura de directoris
 find "$source_dir" -type f -name "*.pdf" -exec cp --parents {} "$destination_dir" \;
 
 # Afegim les programacions extra
-cp -r "$source_hardcoded_dir" "$destination_dir"
+#exec cp -r "$source_hardcoded_dir" "$destination_dir/$source_dir/"
+
+cp -r pdfs_extra/* PDFs/Programacions/
 
 echo "Els PDFs s'han copiat correctament a $destination_dir."
--- genera_pdfs.sh
+++ genera_pdfs.sh
@@ -12,7 +12,8 @@ else
 fi
 
 # Recórrer tots els fitxers 'genera_pdf.py'
-for i in `find . -name genera_pdf.py | grep PMDM`; do
+#for i in `find . -name genera_pdf.py | grep PMDM`; do
+for i in `find . -name genera_pdf.py`; do
     dir_name=$(dirname $i)
     folder_name="${dir_name##*/}"
     output_file="${folder_name}.pdf"
@@ -20,13 +21,16 @@ for i in `find . -name genera_pdf.py | grep PMDM`; do
     error_file="$(pwd)/errors/${folder_name}.txt"
     
     # Canviar al directori correcte abans d'executar el script
-    cd "$dir_name" || exit 1  # Si no es pot canviar de directori, aturem l'script
+    #cd "$dir_name" || exit 1  # Si no es pot canviar de directori, aturem l'script
     
     # Executar el script amb stderr redirigit
-    python3 genera_pdf.py "$output_file" 2>"$error_file" &
+    #python3 genera_pdf.py "$output_file" 2>"$error_file" &
+    bash -c "cd $dir_name && python3 genera_pdf.py $output_file 2>$error_file"
+    #echo "cd $dir_name && python3 genera_pdf.py $output_file"
+    #echo  $error_file
     
     # Tornar al directori original després d'executar el script
-    cd - || exit 1
+    # cd - || exit 1
 done
 
 wait
